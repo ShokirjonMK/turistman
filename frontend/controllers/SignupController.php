517041a5b19616d1155964d03cff0db2746326ae
To`lqin <tulqin940@gmail.com>
2023-04-27 02:31:36 +0500

    ,
    ,

--- frontend/controllers/SignupController.php
+++ frontend/controllers/SignupController.php
@@ -46,7 +46,7 @@ class SignupController extends Controller
             if ($model->load($_POST)) {
 
                 $data = passport(passera($model), passera_num($model), $model->date);
-                if ($data){
+                if ($data !== null){
                     $model->saveData($data);
                     Yii::$app->session->setFlash('success', Yii::t('ui', "Данные созданы успешно"));
                     return $this->redirect(['signup/login']);
